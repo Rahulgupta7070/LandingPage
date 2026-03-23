6e35d46f048720734196c7088a4e52ed8dc22836
Rahul kumar <rahulgupta707062@gmail.com>
2026-03-23 16:13:15 +0530

    ADD router

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import LdPage from "./component/LdPage";
 import './index.css'
 import Navbar from "./component/Navbar";
+import { BrowserRouter,Routes,  Route } from "react-router-dom";
+import Login from "./component/Login";
 
 function App() {
   return (
-    <>
-     <Navbar/>
-     <LdPage/>
-    </>
+  <BrowserRouter>
+      <Navbar/>
+    
+     <Routes>
+      <Route path="/" element={<LdPage/>}></Route>
+      <Route path="/login" element={<Login/>}></Route>
+       
+     </Routes>
+  </BrowserRouter>
+    
+   
   );
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
@@ -10,16 +11,25 @@ function Navbar() {
 
     
       <div className="hidden md:flex space-x-6 text-gray-700 font-medium">
-        <a href="#" className="hover:text-blue-600">Home</a>
+         <NavLink
+          to="/" 
+          className={({ isActive }) =>
+            isActive ? "text-blue-600" : "hover:text-blue-600"
+          }
+        >
+          Home
+        </NavLink>
+
+        <a href="#" className="hover:text-blue-600">Home</a> 
         <a href="#" className="hover:text-blue-600">Features</a>
         <a href="#" className="hover:text-blue-600">About</a>
       </div>
 
-  
+   <Link to="/login">
       <button className="bg-blue-600 text-white px-5 py-2 rounded hover:bg-blue-700 transition">
         Login
       </button>
-
+    </Link>
     </nav>
   );
 }
